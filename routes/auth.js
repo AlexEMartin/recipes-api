535c2776b87688d731a38fc42cf83872cfff521b
Martín <alex.a.martin@accenture.com>
2023-12-20 15:59:55 -0300

    Signup endpoint

--- routes/auth.js
+++ routes/auth.js
@@ -17,7 +17,7 @@ router.post(
 );
 
 router.post(
-  "/register",
+  "/signup",
   [
     check("email", "Email is required").isEmail(),
     check("password", "Password is required").not().isEmpty(),
